2b5480a79de8b5fb4202a3a7917ec1f0fbea8667
Kishore Rachakonda <kishore_personal1@yahoo.com>
2025-05-03 15:31:21 +0530

    Add spec for add method and pass empty string as an argument

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,3 +1,5 @@
 class Calculator
-
+	def add(input)
+		input.to_i
+	end
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,5 +1,7 @@
 require 'calculator'
 
 RSpec.describe Calculator do
-	
+	it "given an empty string returns 0" do
+		expect(Calculator.new.add("")).to eq(0)
+	end
 end
