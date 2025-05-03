2fb847a4450094daa1503b890dc29060c14e736a
Kishore Rachakonda <kishore_personal1@yahoo.com>
2025-05-03 15:36:05 +0530

    Add spec for a single value and returns a number

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe Calculator do
 	it "given an empty string returns 0" do
 		expect(Calculator.new.add("")).to eq(0)
 	end
+
+	it "given a single value returns a number" do
+		expect(Calculator.new.add("1")).to eq(1)
+	end
 end
