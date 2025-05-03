cd2242a4b84236b9e36c52a8372c3a05b4ba2703
Kishore Rachakonda <kishore_personal1@yahoo.com>
2025-05-03 16:18:27 +0530

    Add spec to allow add method to support different delimiters

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -20,4 +20,8 @@ RSpec.describe Calculator do
 	it "handle new lines between numbers (instead of commas)" do
 		expect(Calculator.new.add("1\n2,3")).to eq(6)
 	end
+
+	it "support different delimiters" do
+		expect(Calculator.new.add("//;\n1;2")).to eq(3)
+	end
 end
